a6cf661eb79da5597c540460594cb63944ef42a5
Senne Tijdeman <isenne@outlook.com>
2023-12-04 17:48:02 +0100

    Change deploy

--- deploy.sh
+++ deploy.sh
@@ -27,12 +27,17 @@ kubectl apply -f wordpress/pvc.yaml
 kubectl apply -f proxy/pvc.yaml
 
 echo "Creating ConfigMap's'..."
+kubectl apply -f database/configmap.yaml
 kubectl apply -f proxy/configmap.yaml
 
 echo "Creating secrets from .env..."
 kubectl create secret generic mariadb-secret \
   --from-literal=mysql_root_password="$MYSQL_ROOT_PASSWORD" \
   --from-literal=mysql_password="$MYSQL_PASSWORD"
+
+echo "Deploying Database..."
+kubectl apply -f database/deployment.yaml
+kubectl apply -f database/service.yaml
 
 echo "Deploying WordPress..."
 kubectl apply -f wordpress/deployment.yaml
